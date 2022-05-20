bdf88ccfe3f5944de87010d74baa9537321b98d2
DidiTuga <diogo.diogosnts.santos2@gmail.com>
2022-05-21 00:31:46 +0100

    Modificar já o centroView - parte de testes com separadores!

--- src/Aplicacao/Main.java
+++ src/Aplicacao/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("Login/LoginView.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Registar/RegistarView.fxml"));
         
         Scene scene = new Scene(root);
        
